882dbc3307f567515796a638965487719fbc5c31
seila359 <pablowaltricks@gmail.com>
2026-08-31 11:09:51 -0300

    feat(backend):Criar Repository Unidade2

--- backend/src/main/java/br/senai/projeto/repository/UnidadeRepository.java
+++ backend/src/main/java/br/senai/projeto/repository/UnidadeRepository.java
@@ -1,8 +1,5 @@
 package br.senai.projeto.repository;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +7,5 @@ import br.senai.projeto.entity.Unidade;
 
 @Repository
 public interface UnidadeRepository extends JpaRepository<Unidade, Integer> {
-    
-    Optional<Unidade> findById(Integer id);
 
-    List<Unidade> findAll();
 }
