a4b9495bc67ee07fc89186112ccfa2515d762538
PABLO WALTRICK DOS SANTOS <pablo_w_dos-santos@estudante.sesisenai.org.br>
2026-09-09 10:21:13 -0300

    feature(backend):Bancada

---
+++ backend/src/main/java/br/senai/projeto/entity/Bancada.java
@@ -0,0 +1,43 @@
+package br.senai.projeto.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import br.senai.projeto.entity.StatusConexaoBancada;
+import br.senai.projeto.entity.StatusUsoBancada;
+
+@Entity
+@Table(name = "bancada")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor 
+@Builder
+public class Bancada {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_bancada")
+    private Long idBancada;
+
+    @Column(name = "modelo", nullable = false)
+    private String modelo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_conexao")
+    private StatusConexaoBancada statusConexao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_uso")
+    private StatusUsoBancada statusUso;
+
+    @Column(name = "tempo_base_min")
+    private Integer tempoBaseMin;
+
+    @ManyToOne
+    @JoinColumn(name = "id_unidade", nullable = false)
+    private Unidade unidade;
+
+}
